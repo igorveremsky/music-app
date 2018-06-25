bfcf9b5ca202cb9ba6b570f1de3296e41c54ea82
Igor Veremsky <igorveremsky@gmail.com>
2018-06-25 19:37:32 +0300

    add mysql image

--- api-www/config/db.php
+++ api-www/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=db;dbname=gt_music_app_db',
     'username' => 'root',
-    'password' => '',
+    'password' => 'secretroot',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)

94e0d67b037cfd93df08c58e06da795675ef91fc
Igor Veremsky <igorveremsky@gmail.com>
2018-06-27 16:53:18 +0300

    add adminlte packages and init views for admin panel

--- admin-www/assets/AppAsset.php
+++ admin-www/assets/AppAsset.php
@@ -19,13 +19,7 @@ class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    public $css = [
-        'css/site.css',
-    ];
-    public $js = [
-    ];
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
-    ];
+	public $css = [
+		'//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic',
+	];
 }
--- admin-www/views/layouts/main-login.php
+++ admin-www/views/layouts/main-login.php
@@ -1,5 +1,5 @@
 <?php
-
+use backend\assets\AppAsset;
 use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
@@ -7,8 +7,6 @@ use yii\helpers\Html;
 
 app\assets\AppAsset::register($this);
 dmstr\web\AdminLteAsset::register($this);
-
-$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -16,21 +14,15 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed201
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<?= Html::csrfMetaTags() ?>
+    <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-	<?php $this->head() ?>
+    <?php $this->head() ?>
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="login-page">
+
 <?php $this->beginBody() ?>
-<div class="wrapper">
 
-	<?= $this->render('header.php', ['directoryAsset' => $directoryAsset]) ?>
-
-	<?= $this->render('left.php', ['directoryAsset' => $directoryAsset]) ?>
-
-	<?= $this->render('content.php', ['content' => $content, 'directoryAsset' => $directoryAsset]) ?>
-
-</div>
+    <?= $content ?>
 
 <?php $this->endBody() ?>
 </body>

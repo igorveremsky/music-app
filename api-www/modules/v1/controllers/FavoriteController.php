3d40eb99ed78a6417d60b785f92f3c54d7c67779
Igor Veremsky <igorveremsky@gmail.com>
2018-06-28 19:15:07 +0300

    add search favorite model by query params

--- api-www/modules/v1/controllers/FavoriteController.php
+++ api-www/modules/v1/controllers/FavoriteController.php
@@ -4,6 +4,7 @@ namespace app\modules\v1\controllers;
 
 use app\modules\v1\models\Favorite;
 use Yii;
+use yii\data\ActiveDataProvider;
 
 /**
  * Class FavoriteController
@@ -11,4 +12,44 @@ use Yii;
  */
 class FavoriteController extends DefaultController {
 	public $modelClass = Favorite::class;
+
+	/**
+	 * @return array
+	 */
+	public function actions()
+	{
+		return array_merge(
+			parent::actions(),
+			[
+				'index' => [
+					'class' => 'yii\rest\IndexAction',
+					'modelClass' => $this->modelClass,
+					'checkAccess' => [$this, 'checkAccess'],
+					'prepareDataProvider' => function ($action) {
+						/* @var $model Favorite */
+						$model = $this->modelClass;
+						$query = $model::find();
+
+						$where['user_id'] = Yii::$app->user->getId();
+						if (!empty($queryParam = Yii::$app->request->getQueryParam('model_id'))) {
+							$where['model_id'] = $queryParam;
+						}
+						if (!empty($queryParam = Yii::$app->request->getQueryParam('model_type'))) {
+							$where['model_type'] = $queryParam;
+						}
+
+						$query->where($where);
+
+						if ($query->count() == 1) {
+							return $query->one();
+						}
+
+						$dataProvider = new ActiveDataProvider(['query' => $query]);
+
+						return $dataProvider;
+					}
+				]
+			]
+		);
+	}
 }

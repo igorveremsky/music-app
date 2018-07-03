2bee9ff0bab6fe13e95ff2a603164fc89b7ed71a
Igor Veremsky <igorveremsky@gmail.com>
2018-07-03 16:26:00 +0300

    add test db and infor to readme about tests run

--- api-www/config/test_db.php
+++ api-www/config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=db;dbname=gt_music_app_tests_db';
+$db['dsn'] = 'mysql:host=dbtest;dbname=gt_music_app_test_db';
 
 return $db;
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ docker-compose stop;
 docker-compose up -d --build;
 docker exec -w "//var/www/html/api.gt-music-app.com" app-php composer install;
 docker exec -w "//var/www/html/api.gt-music-app.com" app-php php yii migrate --interactive=0;
+docker exec -w "//var/www/html/api.gt-music-app.com" app-php php ./tests/bin/yii migrate --interactive=0;
 docker exec -w "//var/www/html/api.gt-music-app.com" app-php php yii demo-content/import;
 docker exec -w "//var/www/html/api.gt-music-app.com" app-php php yii elastic-content/index-init;
 docker exec -w "//var/www/html/api.gt-music-app.com" app-php php yii elastic-content/import-from-db;

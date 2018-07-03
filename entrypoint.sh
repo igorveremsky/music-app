0256773ee9711f68cf45e73a8cbe9d53240798d4
Igor Veremsky <igorveremsky@gmail.com>
2018-07-03 15:21:20 +0300

    add installation script

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker-compose stop;
+docker-compose up -d --build;
+docker exec -w "//var/www/html/api.gt-music-app.com" app-php composer install;
+docker exec -w "//var/www/html/api.gt-music-app.com" app-php php yii migrate --interactive=0;
+docker exec -w "//var/www/html/api.gt-music-app.com" app-php php yii demo-content/import;
+docker exec -w "//var/www/html/api.gt-music-app.com" app-php php yii elastic-content/index-init;
+docker exec -w "//var/www/html/api.gt-music-app.com" app-php php yii elastic-content/import-from-db;
+docker exec -w "//var/www/html/admin.gt-music-app.com" app-php composer install;
+read -p "Press any key to continue... " -n1 -s
